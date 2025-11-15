77c6fbfd7a8f9632c10aceefccea6c6f607998c2
Hammad-khann <hk19180@gmail.com>
2025-11-15 18:16:06 +0500

    Yearly saving and compound profit

---
+++ calculator_using_switch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
+    switch (op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if (num2 !=0)
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else cout<<"Num 2 must be non zero"<<endl;
+        break;
+        default:
+
+            cout << "Error! The operator is not correct";
+            break;
+    }
+
+    return 0;
+}
